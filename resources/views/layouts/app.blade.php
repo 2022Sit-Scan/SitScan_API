e6a473f562da83b10eda9ebaa33b961327e30e51
Jose Miguel Fernandez Marin <jose.fernandez@escuelaestech.es>
2022-06-10 19:06:40 +0200

    add productos seeder

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,6 @@
                                 {{ Auth::usuario()->name }}
                             </a>
                                 
-
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
