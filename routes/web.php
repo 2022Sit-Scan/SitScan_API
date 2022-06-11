f8e690bd907c5c0e25d9217fa623097a2e0a02f0
Roberto Navarro Sánchez <robertonavarrosanchez@gmail.com>
2022-06-11 19:53:29 +0200

    rutas

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,14 @@ Route::group(['middleware' => ['auth']], function() {
     Route::get('/', 'Pedido\PedidoProductoController@index');
 
     // definiendo rutas con controlador de recursos
-    Route::resource('usuarios', 'Usuario\UsuarioController');
-     Route::resource('productos', 'Producto\ProductoController');
-     Route::resource('categorias', 'Categoria\CategoriaController');
-     Route::resource('alergenos', 'Alergeno\AlergenoController');
-     Route::resource('cartas', 'Carta\CartaController');
-     Route::resource('mesas', 'Mesa\MesaController');
-     Route::resource('establecimientos', 'Establecimiento\EstablecimientoController');
-     Route::resource('pedidos', 'Pedido\PedidoController');
+    Route::resource('usuarios', 'Usuario\UsuarioController')->except(['show']);
+    Route::resource('productos', 'Producto\ProductoController')->except(['show']);
+    Route::resource('categorias', 'Categoria\CategoriaController')->except(['show']);
+    Route::resource('alergenos', 'Alergeno\AlergenoController')->except(['show']);
+    Route::resource('cartas', 'Carta\CartaController')->except(['show']);
+    Route::resource('mesas', 'Mesa\MesaController')->except(['show']);
+    Route::resource('establecimientos', 'Establecimiento\EstablecimientoController')->except(['show']);
+    Route::resource('pedidos', 'Pedido\PedidoController')->except(['show']);
      Route::resource('pedidosproductos', 'Pedido\PedidoProductoController');
      Route::resource('pedidosacabados', 'Pedido\PedidoAcabadoController');  
 });
