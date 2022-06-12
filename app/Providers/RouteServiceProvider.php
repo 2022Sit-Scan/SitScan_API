f2c51360007247d6c40f4a24136726b650316e7c
Roberto Navarro Sánchez <robertonavarrosanchez@gmail.com>
2022-06-12 09:14:02 +0200

    ruta despues de login

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -22,7 +22,7 @@ class RouteServiceProvider extends ServiceProvider
      * @var string
      */
     
-    public const HOME = '/pedidosProductos';
+    public const HOME = '/pedidosproductos';
 
     /**
      * Define your route model bindings, pattern filters, etc.
